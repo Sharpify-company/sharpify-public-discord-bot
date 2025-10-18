326f7e8fe3862af208b4bbb966f664815d1134b3
rethred <gabrielwastha@gmail.com>
2025-10-17 23:41:32 -0300

    sqlite

--- src/@shared/db/sqlite/_sqlite.ts
+++ src/@shared/db/sqlite/_sqlite.ts
@@ -6,4 +6,52 @@ const sqlite: typeof Database.prototype = new Database(
   path.join(process.cwd(), '/database.db'),
 );
 
+sqlite.prepare(`
+    CREATE TABLE IF NOT EXISTS "discordUsers" (
+    "id"	TEXT,
+    "cartChannelId"	TEXT,
+    "cartMessageId"	TEXT,
+    "cartItems"	TEXT,
+    "couponCode"	TEXT,
+    "subTotalPrice"	REAL,
+    "totalPrice"	REAL,
+    "firstName"	TEXT,
+    "lastName"	TEXT,
+    "email"	TEXT,
+    "gatewayMethod"	TEXT,
+    "cartCreatedAt"	TEXT,
+    PRIMARY KEY("id")
+);
+`).run()
+
+sqlite.prepare(`
+  CREATE TABLE  IF NOT EXISTS "externalEvents" (
+	"id"	TEXT,
+	"eventName"	TEXT,
+	"contextAggregateId"	TEXT,
+	"payload"	TEXT,
+	PRIMARY KEY("id")
+);
+`).run()
+
+sqlite.prepare(`
+CREATE TABLE IF NOT EXISTS "orders" (
+	"id"	TEXT,
+	"orderProps"	TEXT,
+	"customerId"	TEXT,
+	"deliveryStatus"	TEXT,
+	PRIMARY KEY("id")
+);
+`).run()
+
+sqlite.prepare(`
+CREATE TABLE IF NOT EXISTS "products" (
+	"id"	TEXT,
+	"productProps"	TEXT,
+	"channelsLinked"	TEXT,
+	PRIMARY KEY("id")
+);
+`).run()
+
+
 export { sqlite };
--- src/workers/usecases/usecases.module.ts
+++ src/workers/usecases/usecases.module.ts
@@ -1,15 +1,15 @@
-import { Global, Module } from '@nestjs/common';
-import { NecordModule } from 'necord';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { HandleProductEvent } from './handle-product-event';
-import { HandleDeliverToDiscordUserPrivate } from './handle-deliver-to-discord-user-private';
-import { HandleCheckoutEvent } from './handle-checkout-event';
+import { Global, Module } from "@nestjs/common";
+import { NecordModule } from "necord";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { HandleProductEvent } from "./handle-product-event";
+import { HandleDeliverToDiscordUserPrivate } from "./handle-deliver-to-discord-user-private";
+import { HandleCheckoutEvent } from "./handle-checkout-event";
 
 @Global()
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [HandleProductEvent, HandleDeliverToDiscordUserPrivate, HandleCheckoutEvent],
-  exports: [HandleProductEvent, HandleDeliverToDiscordUserPrivate, HandleCheckoutEvent],
+	imports: [],
+	controllers: [],
+	providers: [HandleProductEvent, HandleDeliverToDiscordUserPrivate, HandleCheckoutEvent],
+	exports: [HandleProductEvent, HandleDeliverToDiscordUserPrivate, HandleCheckoutEvent],
 })
 export class UsecasesModule {}

72d2a24521ece647a1344e82de7b44211afd7f2f
rethred <gabrielwastha@gmail.com>
2025-11-06 22:40:04 -0300

    problem with product name leght solved

--- src/bot/products/commands/list-products/auto-complete-product.interceptor.ts
+++ src/bot/products/commands/list-products/auto-complete-product.interceptor.ts
@@ -1,33 +1,33 @@
-import { formatPrice } from '@/@shared/lib';
-import { Sharpify } from '@/@shared/sharpify';
-import { Injectable } from '@nestjs/common';
-import { AutocompleteInteraction } from 'discord.js';
-import { AutocompleteInterceptor } from 'necord';
+import { formatPrice } from "@/@shared/lib";
+import { Sharpify } from "@/@shared/sharpify";
+import { Injectable } from "@nestjs/common";
+import { AutocompleteInteraction } from "discord.js";
+import { AutocompleteInterceptor } from "necord";
 
 @Injectable()
 export class ProductAutocompleteInterceptor extends AutocompleteInterceptor {
-  public async transformOptions(interaction: AutocompleteInteraction) {
-    const focused = interaction.options.getFocused(true);
+	public async transformOptions(interaction: AutocompleteInteraction) {
+		const focused = interaction.options.getFocused(true);
 
-    const req = await Sharpify.api.v1.catalog.product.list({
-      limit: 10,
-      page: 1,
-      title: focused.value.toString(),
-      includeNonListed: true
-    });
-    if (!req.success)
-      return interaction.respond([
-        {
-          name: `Error ao buscar produtos: ${req.errorName}`,
-          value: '0',
-        },
-      ]);
+		const req = await Sharpify.api.v1.catalog.product.list({
+			limit: 10,
+			page: 1,
+			title: focused.value.toString(),
+			includeNonListed: true,
+		});
+		if (!req.success)
+			return interaction.respond([
+				{
+					name: `Error ao buscar produtos: ${req.errorName}`,
+					value: "0",
+				},
+			]);
 
-    return interaction.respond(
-      req.data.products.map((product) => ({
-        name: `${product.info.title}   |  ${formatPrice(product.readonly.lowestPrice)}   |    #${product.shortReference}`,
-        value: product.id,
-      })),
-    );
-  }
+		return interaction.respond(
+			req.data.products.map((product) => ({
+				name: `${product.info.title.slice(0, 40)}   |  ${formatPrice(product.readonly.lowestPrice)}   |    #${product.shortReference}`,
+				value: product.id,
+			})),
+		);
+	}
 }
